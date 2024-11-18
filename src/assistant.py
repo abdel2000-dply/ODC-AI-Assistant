2806f3993dcc5f98dcb7415ea388ae4a2f1e76a1
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-11-18 23:26:59 +0100

    Remove text generation functionality and add error handling for speech playback

--- src/assistant.py
+++ src/assistant.py
@@ -8,7 +8,6 @@ class Assistant:
         """ Initialize the Assistant """
         self.text = text
         self.lang = lang
-        self.generator = pipeline('text-generation', model='gpt2')
         self.qa_model = pipeline('question-answering', model='mrm8488/bert-tiny-5-finetuned-squadv2')
 
     def generate_speech(self):
@@ -19,19 +18,17 @@ class Assistant:
         return output_file
 
     def play_speech(self):
-        """ Play the speech """
-        output_file = self.generate_speech()
-        os.system(f"mpg321 {output_file}")
+        try:
+            output_file = self.generate_speech()
+            os.system(f"mpg321 {output_file}")
+        except Exception as e:
+            print(f"Error during playback: {e}")
+
 
     def change_language(self, lang):
         """ Change the language of the speech """
         self.lang = lang
         return self.lang
-
-    def ask_AI(self, prompt):
-        """ Ask AI a question and get a response """
-        response = self.generator(prompt, max_length=50, num_return_sequences=1, truncation=True)
-        return response[0]['generated_text'].strip()
 
     def ask_factual_question(self, question, context):
         """ Ask a factual question and get a response """
