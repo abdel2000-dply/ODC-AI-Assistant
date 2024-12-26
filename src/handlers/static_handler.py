29a51fe0dfc7777dcce579ecf52c2d5b9c60ac9f
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-12-26 13:05:15 +0100

    update

--- src/handlers/static_handler.py
+++
@@ -1,9 +0,0 @@
-import json
-
-class StaticHandler:
-    def __init__(self, static_responses_file='static_responses.json'):
-        with open(static_responses_file, 'r') as file:
-            self.static_responses = json.load(file)
-
-    def get_response(self, question):
-        return self.static_responses.get(question)
