a3a2a38d0629dfc58b6ac1778af586858f238580
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2025-02-06 15:58:05 +0100

    Remove Arabic language option from the AssistantUI languages list

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ class AssistantUI(BoxLayout):
     is_recording = BooleanProperty(False)
     visualizer_angle = NumericProperty(0)  # For the recording animation
 
-    languages = ['English', 'French', 'Arabic']
+    languages = ['English', 'French']
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
